b712f542760bb024bdee7b99edf52fe79cf80bbd
Deivid Rocha <deividzor@gmail.com>
2024-07-10 15:18:47 -0300

    primeiro commit

---
+++ src/test/java/dvd/com/forum_hub/ForumHubApplicationTests.java
@@ -0,0 +1,13 @@
+package dvd.com.forum_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
